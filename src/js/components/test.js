894e21e5185c72b3ffce1c88b6b312c884c3e04c
Winstons11 <75261174+Winstons11@users.noreply.github.com>
2022-08-11 19:12:46 +0300

    Add files via upload

---
+++ src/js/components/test.js
@@ -0,0 +1,26 @@
+// const buttons = document.querySelectorAll(".btn");
+//
+// const handeleClick = (event) => {
+//   console.log(event.target);
+//   console.log("curtarget", event.currentTarget);
+//   // event.stopPropagation()
+// };
+//
+// window.addEventListener(
+//   "click",
+//   () => {
+//     console.log("Window Click");
+//     event.stopPropagation();
+//   },
+//   { capture: true }
+// );
+//
+// buttons.forEach((button) => {
+//   button.addEventListener("click", handeleClick);
+// });
+//
+// const img = document.querySelector("img");
+// img.addEventListener("mouseover", (event) => {
+//   console.log(event.currentTarget);
+//   console.log(this);
+// });
